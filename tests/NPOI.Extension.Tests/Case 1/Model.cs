f2eb434ee681e72c15170fb7d0635bf6de995238
Christian Junk <christian.junk@amecfw.com>
2016-08-10 18:31:56 +0200

    Added NUnit-Tests project.

---
+++ tests/NPOI.Extension.Tests/Case 1/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NPOI.Extension.Tests.Case_1
+{
+    public class Model
+    {
+        [Column(AutoIndex = true, Title = "F")]
+        public string BuildingName { get; set; }
+    }
+}
---
+++ tests/NPOI.Extension.Tests/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NPOI.Extension.Tests.Case_1;
+
+namespace NPOI.Extension.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Case 1\Book - Testcase 1.xlsx");
+            Assert.IsTrue(File.Exists(file));
+
+            var loadFromExcel = Excel.Load<Model>(file);
+            Debug.WriteLine(loadFromExcel.Count());
+        }
+    }
+}
